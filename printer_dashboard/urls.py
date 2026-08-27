58c34aa093db2db7801c531e631eadc674b3f85a
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2026-08-27 12:14:53 +0200

    Add user view to printer_dashboard (refactoring)

--- printer_dashboard/urls.py
+++ printer_dashboard/urls.py
@@ -25,14 +25,15 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
-from printer_dashboard.view_csrf import csrf
+from printer_dashboard.views import csrf, user
 
 urlpatterns = [
     path(".admin/", admin.site.urls),
     path("api/", SpectacularSwaggerView.as_view(), name="swagger-ui"),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api-auth/csrf/", csrf),
+    path("api-auth/csrf/", csrf.csrf),
+    path("api-auth/user/", user.UserView.as_view()),
     path("dashboard/", include("dashboard.urls")),
     path("core/", include("core.urls")),
 ]
